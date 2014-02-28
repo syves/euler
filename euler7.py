304a3953f77b0fb7e4db6b00e50451029146d4b9
Shakrah Yves <7shakrahs@gmail.com>
2014-02-27 23:00:11 -0800

    euler.8.py

---
+++ euler7.py
@@ -0,0 +1,19 @@
+'''By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?'''
+
+def isprime(n):
+    for m in range(2, n):
+        if n % m == 0:
+            return false
+        return True
+
+def getnthprime(num):
+n = 1, factors = [], num = (10e3)+1
+    while factors.len <= num:
+        n + 1
+        if isprime:
+            factors.append(n)
+    return factors[-1]
+    
+print getnthprime(10001) 
