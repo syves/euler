4c47dae50d90237aa41e9f4ef046c2fe42b18e31
Shakrah Yves <7shakrahs@gmail.com>
2014-02-28 11:17:28 -0800

    euler10.py

--- euler10.py
+++ euler10.py
@@ -1,3 +1,27 @@
 '''The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
-Find the sum of all the primes below two million.
+Find the sum of all the primes below two million.'''
+
+import operator
+
+def isprime(num):
+    for n in range(2, num):
+        if num % n == 0:
+            return False
+    return True
+    
+'''def factorize(num):
+    for n in range(2, num):
+        if num % n == 0:
+            return [n] + factorize(num / n)
+    return [num]
+print factorize(49)'''
+
+
+def sumprimes(num):
+    return reduce(operator.__add__ ,filter(isprime, range(2, (num) + 1)))
+
+
+print sumprimes(2000)
+
+
