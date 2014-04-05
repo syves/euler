73587327077f80c2dc21a065105b28d25f405f54
Shakrah Yves <7shakrahs@gmail.com>
2014-04-04 23:12:47 -0700

    euler12 solved

--- euler4.js
+++ euler4.js
@@ -3,10 +3,15 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
 var isPal = function(str) {
+
+/*equality test between on str "121 and split ours string into single char
+"1", "2", "1", reverse them "1", "2", "1" and join the "121"*/
+
   return str === str.split("").reverse().join("");
 };
 
 var largestPalindrome = function(numDigits) {
+//raised to pow -1 ?
   var min = Math.pow(10, numDigits - 1);
   var max = Math.pow(10, numDigits);
   var results = [];
