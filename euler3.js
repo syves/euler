d0697c885f4ecf2bfaa8654271ef79a39f32b314
syves <7shakrahs@gmail.com>
2014-12-05 21:04:25 -0800

    euler3 optimized for speed

--- euler3.js
+++ euler3.js
@@ -1,36 +1,31 @@
 //The prime factors of 13195 are 5, 7, 13 and 29.
 
 //What is the largest prime factor of the number 600851475143 ?
-  //
+  //tests for prime numbe
   var isPrime = function(num){
     for (var n= 2; n < num; n++){
       if (num % n === 0) {
         return false
-      } else {
-        return true
+      }
     }
+    return true
 }
-
-isPrime(5)
-
-5 % 2 > 0 && 5 % 3 > 0 && 5 % 4 > 0
-
-isPrime(6)
-
-6 % 2 > 0 && 6 % 3 > 0 && 6 % 4 > 0 && 6 % 5 > 0
-
-
+//number // num(which is smallest prime factor) == largest prime
 var getLargestPrime = function(number) {
-  var num = 1
-  while (num < number) {
-    var largestPrime = 0;
-    if (num % 2 != 0) {
-      largestPrime = num;
-      num += 1;
+  var sqrt = Math.sqrt(number), smallFactors = [];
+  for (var small_num = 2; small_num <= sqrt; small_num++){
+    if (number % small_num === 0){
+      if (isPrime(number / small_num)) {
+        return number/small_num;
+      }
+      smallFactors.push(small_num)
     }
   }
-  console.log(largestPrime)
-  return largestPrime
+  for (var idx = smallFactors.length - 1; idx >= 0; idx--){
+    if (isPrime(smallFactors[idx])) {
+      return smallFactors[idx];
+    }
+  }
 }
 
-//console.log(getLargestPrime(13195))
+console.log(getLargestPrime(600851475143))
