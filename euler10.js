1047569422f1ea064ea1a017da4338a98f525cfa
syves <7shakrahs@gmail.com>
2014-12-08 15:54:00 -0800

    euler10.js initial solution too slow

---
+++ euler10.js
@@ -0,0 +1,26 @@
+//The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+//Find the sum of all the primes below two million.
+//
+var isPrime = function(number){
+  for (var n = 2; n < number; n++){
+    if (number % n === 0){
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(5))
+console.log(isPrime(6))
+
+  var sumPrimes = function(stop){
+    var sum = 0;
+    for (var m = 2; m <= stop; m++){
+      if (isPrime(m)){
+        sum += m;
+      }
+    }
+    return sum
+  }
+console.log(sumPrimes(1000000))
