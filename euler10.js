d90f7cffb96c1866f675f0f224438bfe03178457
syves <7shakrahs@gmail.com>
2014-12-09 15:24:35 -0800

    euler10.js, soo fast, soo odd

--- euler10.js
+++ euler10.js
@@ -2,24 +2,75 @@
 
 //Find the sum of all the primes below two million.
 //
-var isPrime = function(number){
-  for (var n = 2; n < number; n++){
-    if (number % n === 0){
+var isPrime = function(number, smallerPrimes){
+  for (var idx = 0; idx < smallerPrimes.length; idx++){
+    if (number % smallerPrimes[idx] == 0){
       return false;
     }
   }
   return true;
 }
 
-var sumPrimes = function(stop){
-  var sum = 0;
+var sumPrimes_ = function(stop){
+  var sum = 0, primes = [];
   for (var m = 2; m <= stop; m++){
-    if (isPrime(m)){
+    if (isPrime(m, primes)){
+      primes.push(m);
       sum += m;
-      }
     }
-    return sum
   }
-console.log(sumPrimes(2000))
+  return sum;
+}
+//console.log(sumPrimes(2000000))
 
 //30 min. initial solution dec.8th
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function sumPrimes(stop) {
+
+  var state = [true, true];  // 0 and 1 are not prime
+  for (var n = state.length; n < stop; n++) {
+    state[n] = false;
+  }
+
+  for (var n = 2; n < stop; n++) {
+    if (!state[n]) {
+      for (var m = n + n; m < stop; m += n) {
+        state[m] = true;
+      }
+    }
+  }
+  return state.reduce(function(sum, marked, n) {
+    return marked ? sum : sum + n;
+  },0);
+}
+console.log(sumPrimes(2000000))
+
+
