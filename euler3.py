d262551f255111c8aec97ffc12640b6edc90e260
syves <7shakrahs@gmail.com>
2015-01-07 16:41:31 +1300

    euler3.py reafctored with filter, and reducing pool with sqrt

--- euler3.py
+++ euler3.py
@@ -1,26 +1,32 @@
-#!/usr/bin/env python
-# Problem 3 - Largest prime factor
-# The prime factors of 13195 are 5, 7, 13 and 29.
-# What is the largest prime factor of the number 600851475143 ?
-
 import math
 
-def prime(n):
-    # print "In prime with %s" % n
-    # sqrt_n = math.sqrt(n)
-    # ceil_sqrt_n = math.ceil(sqrt_n)
-    #print "Ceil... %s" % ceil_sqrt_n
-    for x in range(2, n + 1):
+def isprime(n):
+    ceil_sqrt_n = math.ceil(math.sqrt(n))
+    for x in range(2, int(ceil_sqrt_n)):
         if n % x == 0:
             return False
     return True
 
-n = 13195
-sqrt_n = math.sqrt(n)
-ceil_sqrt_n = math.ceil(sqrt_n)
+def factorize(n):
+    """
+    Returns a set of factors of n.
+    """
+    ceil_sqrt_n = math.ceil(math.sqrt(n))
+    factors = []
+    for x in range(2, int(ceil_sqrt_n)):
+        if ( n % x == 0):
+            #largeFactor = n / x
+            factors.append(x)
+            factors.append(n/x)
+    return factors
 
-for x in range(2, int(ceil_sqrt_n) + 1):
-    if n % x == 0 and prime(x):
-        print x
-        pass
+def euler3(n):
+    """
+    Problem 3 - Largest prime factor
+    The prime factors of 13195 are 5, 7, 13 and 29.
+    What is the largest prime factor of the number 600851475143?
+    """
+    return max(filter(isprime, factorize(n)))
+
+print euler3(600851475143)
 
